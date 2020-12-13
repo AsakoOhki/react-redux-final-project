3d06140b09a6f0f65f0d98b767a86679ed3d198b
Asako Ohki <ohkiasako@gmail.com>
2020-12-12 20:21:12 -0800

    table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
-import ToDoList from "./components/TodoList";
+// import ToDoList from "./components/TodoList";
 import "./styles.css";
 import { connect } from "react-redux";
 
 function App({ toDoList }) {
   return (
     <div className="container">
-      <h1 className="center blue-text">ToDo's ({toDoList.length})</h1>
+      <h1 className="center blue-text">Angular 9 MatTable CRUD Example</h1>
       <ToDoList />
     </div>
+  );
+}
+
+const ToDoList = () => {
+  return (
+    <table>
+      <tr>
+        <th>id</th>
+        <th>Title</th>
+        <th>State</th>
+        <th>Url</th>
+        <th>Created At</th>
+        <th>Updated At</th>
+        <th>+</th>
+      </tr>
+      <tr>
+        <td>id</td>
+        <td>Title</td>
+        <td>State</td>
+        <td>Url</td>
+        <td>Created At</td>
+        <td>Updated At</td>
+        <td>+</td>
+      </tr>
+
+    </table>
   );
 }
 
